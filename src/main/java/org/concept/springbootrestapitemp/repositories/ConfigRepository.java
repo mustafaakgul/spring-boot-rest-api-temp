2548f69007b5a45522579574bfcbc36d5bb3e5db
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-28 03:23:36 +0300

    Add exceptions

--- src/main/java/org/concept/springbootrestapitemp/repositories/ConfigRepository.java
+++ src/main/java/org/concept/springbootrestapitemp/repositories/ConfigRepository.java
@@ -2,6 +2,8 @@ package org.concept.springbootrestapitemp.repositories;
 
 import org.concept.springbootrestapitemp.models.Config;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConfigRepository extends CrudRepository<Config, Long> {
 }
