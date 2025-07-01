3eee86f8bfefa7397d4d3193524cfa94c97412c2
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-07-01 21:50:22 +0300

    Add User module

--- src/main/java/org/concept/springbootrestapitemp/controllers/UserController.java
+++ src/main/java/org/concept/springbootrestapitemp/controllers/UserController.java
@@ -2,10 +2,12 @@ package org.concept.springbootrestapitemp.controllers;
 
 import org.concept.springbootrestapitemp.exceptions.UserNotFoundException;
 import org.concept.springbootrestapitemp.models.User;
+import org.concept.springbootrestapitemp.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +15,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping(path = "api/v1/users")
 public class UserController {
 
-    private static Map<String, User> users = new HashMap<>();
+    /*private static Map<String, User> users = new HashMap<>();
     static {
         User temp1 = new User("1", "John", "Doe");
         User temp2 = new User("2", "Jane", "Doe");
         users.put(temp1.getId(), temp1);
         users.put(temp2.getId(), temp2);
+    }*/
+    private final UserRepository userRepository;
+
+    public UserController (UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @ExceptionHandler(value = UserNotFoundException.class)
@@ -31,7 +40,12 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @RequestMapping(path = "/users/{id}", method = RequestMethod.PUT)
+    @GetMapping("")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    /*@RequestMapping(path = "/users/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Object> updateUser(@PathVariable String id, @RequestBody User user) {
         if (!users.containsKey(id)) {
             throw new UserNotFoundException();
@@ -40,5 +54,5 @@ public class UserController {
         user.setId(id);
         users.put(id, user);
         return new ResponseEntity<>("User is updated successfully.", HttpStatus.OK);
-    }
+    }*/
 }
--- src/main/java/org/concept/springbootrestapitemp/models/User.java
+++ src/main/java/org/concept/springbootrestapitemp/models/User.java
@@ -1,33 +1,81 @@
 package org.concept.springbootrestapitemp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    // private String username;
+
     private String firstName;
+
     private String lastName;
 
     public User() {
     }
 
-    public User(String id, String firstName, String lastName) {
-        this.id = id;
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
